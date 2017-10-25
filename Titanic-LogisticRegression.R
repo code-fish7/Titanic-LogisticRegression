d00d60c8775d4a2ce61455b0ae280ef71f036026
Venkataraman Balasubramanian (VB) <venkataraman.bal@gmail.com>
2017-10-25 19:37:12 -0400

    Updated the code

--- Titanic-LogisticRegression.R
+++ Titanic-LogisticRegression.R
@@ -1,32 +1,66 @@
 #Predicting Titanic survival rate using logistic regression
 library(caret)
 library(mice)
+library(caTools)
+library(ggplot2)
 
 #Load the training data
-setwd('D:\\Venkat\\Villanova\\Data Science Projects\\Titanic-LogisticRegression')
-dataset_full = read.csv('train.csv')
+#setwd('Provide the folder name which contains train.csv')
+titanic = read.csv('train.csv')
+names(titanic)
 
-#Reset the order of features in the dataset
-dataset_full = dataset_full[c(1,3,4,5,6,7,8,9,10,11,12,2)]
+#Select the variables of interest
+titanic = titanic[,c(2,3,5,6,7,8,10)]
+names(titanic)
 
-#Pick the features of interest
-dataset = dataset_full[,-c(1,3,8,10,11)]
+#Determing the modeling type of selected variables
+str(titanic)
 
-#Redefine the modeling types of the features
-str(dataset)
-dataset$Pclass = factor(x = dataset$Pclass)
-dataset$SibSp = factor(x = dataset$SibSp)
-dataset$Parch = factor(x = dataset$Parch)
-dataset$Survived = factor(dataset$Survived)
+#Change the modeling type of Survived and Pclass
+titanic$Survived = factor(titanic$Survived)
+titanic$Pclass = factor(titanic$Pclass, 
+                        levels = c(3,2,1),
+                        labels = c(3,2,1),
+                        ordered = TRUE)
+str(titanic)
 
-#Check for missing data
-md.pattern(dataset)
-impute_dataset = mice(data = dataset,
-                      m = 1,
+#Look for missing values and impute missing values
+md.pattern(titanic)
+
+#Impute missing Age feature using mice package
+titanic_impute = mice(data = titanic,
+                      m = 1, 
                       maxit = 3)
-imputed_dataset = complete(impute_dataset,1)
+titanic_imputed = complete(titanic_impute,1)
 
-#Scale continuous features
-str(imputed_dataset)
-imputed_dataset$Age = scale(imputed_dataset$Age)
-imputed_dataset$Fare = scale(imputed_dataset$Fare)
+#Scale the continuous features
+titanic_imputed[,c(4,5,6,7)] = scale(titanic_imputed[,c(4,5,6,7)])
+
+#Split the data into training and test set
+set.seed(5)
+split = sample.split(Y = titanic_imputed$Survived,
+                     SplitRatio = 0.8)
+training_set = subset(titanic_imputed,
+                      split == TRUE)
+test_set = subset(titanic_imputed,
+                  split == FALSE)
+
+#Build the logistic regression classifier using the training set
+titanic_logreg = glm(formula = Survived ~ .,
+                     data = training_set,
+                     family = 'binomial')
+
+#Predict the survival in test set using the logistic regression classifier
+y_prob = predict(titanic_logreg,
+                 newdata = test_set[,-1],
+                 type = 'response')
+y_pred = ifelse(y_prob >= 0.5, 1, 0)
+
+#Build the confusion matrix
+cm = table(test_set[,1], y_pred)
+cm
+
+#Calculate the misclassification rate
+misclassification_rate = (cm[2,1] + cm[1,2])/ (cm[1,1] + cm[1,2] + cm[2,1] + cm[2,2])
+misclassification_rate
+
